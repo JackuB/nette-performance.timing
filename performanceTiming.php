bb2b7b430dc0c61f79cefd353aa932df55617a48
Jakub Mikulas <jakub@jedenbod.cz>
2013-01-21 01:41:46 +0100

    Initial

---
+++ performanceTiming.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * performance.timing for Debugger Bar Panel
+ * @author Jakub Mikuláš <jakub@jedenbod.cz>
+ * @see https://github.com/JackuB/nette-performance.timing
+ */
+final class performanceTiming extends \Nette\Application\UI\Control implements \Nette\Diagnostics\IBarPanel
+{
+	/**
+	 * Html code for Debugger Bar Panel
+	 * @author Jakub Mikuláš <jakub@jedenbod.cz>
+	 * @return string
+	 */
+	public function getTab()
+	{
+		$template = $this->getFileTemplate(__DIR__.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR.'tab.latte');
+		return $template;
+	}
+
+	/**
+	 * Html code for Debugger Bar Panel
+	 * @author Jakub Mikuláš <jakub@jedenbod.cz>
+	 * @return string
+	 */
+	public function getPanel()
+	{
+		$template = $this->getFileTemplate(__DIR__.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR.'panel.latte');
+		return $template;
+	}
+}
